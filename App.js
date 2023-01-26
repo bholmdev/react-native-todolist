bd6375c2e086fbe7f64ff33b6d3471bf74731b69
Brandon Holman <bholmdev@gmail.com>
2023-01-26 13:12:09 -0600

    Part 1 complete

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from "./app/components/Task"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
+          <Task text={"Task 3"} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e8eaed'
+  },
+  items: {
+    marginTop: 30
+  },  
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
   },
 });
